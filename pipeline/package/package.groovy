209d5ba7febc955a905cfd1831e5e9df9a0bab63
Bob Clarke <bclarke1@sapient.com>
2018-03-10 21:09:41 +0000

    Remove step syntax

--- pipeline/package/package.groovy
+++ pipeline/package/package.groovy
@@ -1,28 +1,6 @@
-
 def pack( String targetEnv ){
-
-  //def applicationServerEnv = context.config.application.SERVER_ENV ?: 'production'
-
-  node(){
-
-    def versionNumber = env.BUILD_NUMBER
-    //def packageDir = context.config.package.directory
-
-    try {
-      withEnv([
-	"environment=${targetEnv}",
-      ]) {
-        echo "This is package.groovy"
-      }
-    } catch (error) {
-      echo "FAILURE: Application Build failed"
-      throw error
-    } finally{
-      step([
-        //$class: 'WsCleanup', 
-        //notFailBuild: true
-      ])
-    }
-  }
+	node(){
+        	echo "This is package.groovy"
+    	}
 }
 return this;
