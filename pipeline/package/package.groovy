e4a9b156e84c26166da6d6325d045273ffecb18a
Bob Clarke <bclarke1@sapient.com>
2018-03-10 21:25:39 +0000

    adding checkout to package.groovy

--- pipeline/package/package.groovy
+++ pipeline/package/package.groovy
@@ -1,6 +1,7 @@
 def pack( String targetEnv ){
 	node(){
    		echo "This is package.groovy"
+		checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/bobclarke/hw.git']]])
     }
 }
 return this;
