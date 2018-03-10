b41630c80ed3a048b27171683dda77a764a404eb
Bob Clarke <bclarke1@sapient.com>
2018-03-10 21:28:38 +0000

    Formatting

--- pipeline/package/package.groovy
+++ pipeline/package/package.groovy
@@ -1,7 +1,14 @@
 def pack( String targetEnv ){
 	node(){
    		echo "This is package.groovy"
-		checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/bobclarke/hw.git']]])
+		checkout([
+			$class: 'GitSCM', 
+			branches: [[name: '*/master']], 
+			doGenerateSubmoduleConfigurations: false, 
+			extensions: [], 
+			submoduleCfg: [], 
+			userRemoteConfigs: [[url: 'https://github.com/bobclarke/hw.git']]
+		])
     }
 }
 return this;
